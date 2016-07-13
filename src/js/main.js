0441983d5834d9d1d779e7a82f4bd95b6602825c
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-13 17:31:23 -0400

    Created game board in main.js and styled using main.scss. Also installed jQuery.

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+import $ from "jquery";
+
+for (var x=0; x < 100; x++) {
+	$(".board").append(`<div class="square ${x}">
+		</div>
+		`)
+};
+
+$(".95").append(`<div id="player">
+	<img src="http://placecage.com/50/50">
+	</div>`);
