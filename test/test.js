63583332c1f7f7d0698404e8c45802369b65009a
Will Johnson <williamterryjohnsonjr@gmail.com>
2016-07-14 12:56:23 -0400

    Added new tests for Player/Zombie Classes

--- test/test.js
+++ test/test.js
@@ -29,12 +29,44 @@ describe('Player Class', function () {
   	it('should have an image', () =>{
   		expect(player.image).to.be.a('string');
   	});
-  	it('should have an image for when it is bitten', ()=>{
+  	it('should have an image for when it is bitten once', ()=>{
   		expect(player.biteImage).to.be.a('string');
+  	});
+  	it('should have an image for when it is bitten twice', ()=>{
+  		expect(player.biteImage2).to.be.a('string');
   	});
   	it('should have a sound for when it is bitten', () => {
   		expect(player.sound).to.be.a('string');
   	});
+  	it('should have a sound for when it dies', ()=>{
+  		expect(player.deathScream).to.be.a('string');
+  	});
   });
+});
 
+describe('Zombie Class', function(){
+	let zombie;
+	beforeEach(()=>{
+		zombie = new Zombie();
+	});
+	describe ('Zombie Class Creation', ()=>{
+		it('should have 3 hitpoints',()=>{
+  			expect(zombie.hitpoints).to.equal(3);
+  		});
+	  	it('should have an image', () =>{
+	  		expect(zombie.image).to.be.a('string');
+	  	});
+	  	it('should have an image for when it gets shot', ()=>{
+	  		expect(zombie.shotImage).to.be.a('string');
+	  	});
+	  	it('should have an image for when it gets shot twice', ()=>{
+	  		expect(zombie.shotImage2).to.be.a('string');
+	  	});
+	  	it('should have a sound for when it is shot', () => {
+	  		expect(zombie.sound).to.be.a('string');
+	  	});
+	  	it('should have a sound for when it dies', ()=>{
+	  		expect(zombie.deathScream).to.be.a('string');
+	  	});
+	});
 });
